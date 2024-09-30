2a1f4273d7f4b94e8141b017af4e212b4ca01f26
luisdev338 <luisfemipejazz90@gmail.com>
2024-09-30 06:42:21 -0300

    java.dom

---
+++ dom.java/javascript-turma-devfullstack3-main/objetodoconsole/js/script.js
@@ -0,0 +1,18 @@
+//Exibindo uma mensagem de aviso na console
+console.warn("Atenção: Aviso!");
+
+//Exibindo uma mensagem de erro na console
+console.error("Ocorreu erro");
+
+//Exibindo uma mensagem informativa na console
+console.info("Informativa");
+
+//Criando um grupo de mensagens na console
+console.group("Mensagens de Teste");
+console.log("Atente-se ao passo 1");
+console.log("Execute o passo 2");
+console.log("Execute o passo final");
+console.groupEnd();
+
+//Limpar o console
+//console.clear();
